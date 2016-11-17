e8468d08ca197cfa7e1de7a6da4a335be901e0bc
perecastors <58650@supinfo.com>
2016-11-17 13:46:06 +0100

    MAJ Table Commandes et Sauces

--- app/models/Commandes.java
+++ app/models/Commandes.java
@@ -10,7 +10,22 @@ import javax.persistence.Entity;
 @Entity
 public class Commandes extends Model{
 
+    //infos client
     public String nom;
     public String adresse;
+    public String codePostal;
+    public String ville;
     public String telephone;
+    //infos pizza
+    public String taille;
+    public String pates;
+    public String sauce;
+    public String garniture1;
+    public String garniture2;
+    public String garniture3;
+    public String garniture4;
+    public String garniture5;
+    //infos prix
+    public int prix;
+
 }
---
+++ app/models/Sauces.java
@@ -0,0 +1,11 @@
+package models;
+
+import play.db.jpa.Model;
+
+/**
+ * Created by Formation on 17/11/2016.
+ */
+public class Sauces extends Model {
+
+    public String sauce;
+}
